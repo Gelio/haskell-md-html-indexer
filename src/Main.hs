66be17c26d87de0fba2ef3684cdf9f2c19b9497d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-04 13:03:54 +0200

    add compressed readme example, add proper file indexing

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,30 @@
 module Main where
 
 import Conduit
-import Data.Text (Text)
+import Control.Monad
+import Data.Text (Text, singleton, pack)
+import qualified Data.Text as T
+import System.IO
 
 import IndexerOptions
 import HeadingExtract
 
 search :: SearchOptions -> FilePath -> IO ()
-search _ _ = putStrLn "Search"
+search (SearchOptions phrase exec) input = putStrLn "Search"
 
 index :: IndexOptions -> FilePath -> IO ()
-index _ _ = putStrLn "Index"
+index (IndexOptions rs) output = withFile output WriteMode (\h -> mapM_ (indexResource h) rs)
+  where
+    indexResource :: Handle -> String -> IO ()
+    indexResource handle resource = runConduitRes
+      $ getResourceHeadingsTrimmed resource
+      .| mapC (insertResourcePath $ pack resource)
+      .| unlinesC
+      .| encodeUtf8C
+      .| sinkHandle handle
+
+    insertResourcePath :: Text -> Text -> Text
+    insertResourcePath resource heading = T.concat [resource, singleton '\ETB', heading]
 
 performCommand :: Options -> IO ()
 performCommand (Options (Search o) path) = search o path
