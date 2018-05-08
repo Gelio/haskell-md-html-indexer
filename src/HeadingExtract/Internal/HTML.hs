414e5277e62b212adc6c9c75c2901bc7babffa04
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-08 20:52:08 +0200

    begin refactor

--- src/HeadingExtract/Internal/HTML.hs
+++ src/HeadingExtract/Internal/HTML.hs
@@ -8,7 +8,8 @@ import           Data.Text             (Text, unpack)
 import           Data.XML.Types
 import           Text.HTML.DOM
 
-getHeadingsFromHTML :: ConduitT ByteString Text (ResourceT IO) ()
+-- getHeadingsFromHTML :: ConduitT ByteString Text (ResourceT IO) ()
+getHeadingsFromHTML :: MonadResource m => ConduitT ByteString Text m ()
 getHeadingsFromHTML = eventConduit .| filterHeadings .| getHeadingText
 
 getHeadingText :: Monad m => ConduitT Event Text m ()
