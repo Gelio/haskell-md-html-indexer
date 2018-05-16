56e814da98c42dc275eb87387da55934d1f04df6
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-16 22:17:26 +0200

    import Show for UnknownExtensionException

--- src/HeadingExtract/Exception.hs
+++ src/HeadingExtract/Exception.hs
@@ -2,6 +2,9 @@ module HeadingExtract.Exception (UnknownExtensionException(..)) where
 
 import Control.Exception (Exception)
 
-newtype UnknownExtensionException = UnknownExtensionException String deriving Show
+newtype UnknownExtensionException = UnknownExtensionException String
+
+instance Show UnknownExtensionException where
+  show (UnknownExtensionException ext) = "Unknown extension " ++ ext
 
 instance Exception UnknownExtensionException
