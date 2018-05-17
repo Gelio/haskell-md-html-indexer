44d91027d8ed98e1b3d491eaf500906abcc18a40
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-17 21:52:25 +0200

    enable concurrently indexing resources

--- README.md
+++ README.md
@@ -27,4 +27,20 @@ hfmt src -w
 hlint src
 ```
 
-## example headings
+## Example commands to test for concurrency
+
+### Test for not interleaved output
+
+`index.idx` should have a correct structure. The output cannot be interleaved.
+
+```bash
+stack exec md-html-indexer -- index https://stackoverflow.com/questions/857653/get-a-list-of-urls-from-a-site http://hackage.haskell.org/package/base-4.11.1.0/docs/System-IO-Unsafe.html https://hackage.haskell.org/package/base-4.9.1.0/docs/Control-Concurrent-MVar.html http://hackage.haskell.org/package/base-4.11.1.0/docs/Control-Concurrent.html http://hackage.haskell.org/package/parallel-3.1.0.1/docs/Control-Parallel-Strategies.html http://hackage.haskell.org/package/deepseq-1.4.3.0/docs/Control-DeepSeq.html#v:force http://book.realworldhaskell.org/read/concurrent-and-multicore-programming.html http://hackage.haskell.org/package/stm-conduit-4.0.0/docs/Data-Conduit-Async.html https://docs.expo.io/versions/latest/guides/push-notifications https://facebook.github.io/react-native/docs/asyncstorage.html#getitem https://www.abeautifulsite.net/downloading-a-list-of-urls-automatically https://developers.google.com/search/docs/guides/create-URLs https://developers.google.com/search/docs/guides/enhance-site https://en.wikipedia.org/wiki/Bandwidth_throttling
+```
+
+### Test for parallelism
+
+The last two pages should be indexed (almost) immediately.
+
+```bash
+stack exec md-html-indexer -- index https://en.wikipedia.org/wiki/Bandwidth_throttling https://httpstat.us/200?sleep=10000  https://developers.google.com/search/docs/guides/enhance-site
+```
--- src/Index.hs
+++ src/Index.hs
@@ -6,17 +6,50 @@ import           Conduit
 import           Data.Text      (Text, pack, singleton)
 import qualified Data.Text      as T
 import           System.IO
+import           System.IO.Unsafe (unsafePerformIO)
+import Control.Monad (when)
+import Control.Concurrent (forkOS, ThreadId)
+import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar, newMVar, MVar)
 
 import           Exception      (handleIOError)
 import           HeadingExtract
 import           IndexerOptions (IndexOptions (..))
 
+flushIndexedResults :: Bool
+flushIndexedResults = True
+
 -- TODO: append to index (as option)
--- TODO: index each file concurrently
 index :: IndexOptions -> FilePath -> IO ()
 index (IndexOptions rs) output = do
-  withFile output WriteMode (\h -> mapM_ (safeIndexResource h) rs)
+  withFile output WriteMode (concurrentlyIndexResources rs)
   putStrLn $ "Updated index at " ++ output
+
+concurrentlyIndexResources :: [String] -> Handle ->IO ()
+concurrentlyIndexResources rs h = do
+  mapM_ (forkChild . safeIndexResource h) rs
+  waitForChildren
+
+  where
+    -- Adapted from http://hackage.haskell.org/package/base-4.11.1.0/docs/Control-Concurrent.html#g:12
+    children :: MVar [MVar ()]
+    children = unsafePerformIO (newMVar [])
+
+    waitForChildren :: IO ()
+    waitForChildren = do
+      cs <- takeMVar children
+      case cs of
+        [] -> return ()
+        m:ms -> do
+          putMVar children ms
+          takeMVar m
+          waitForChildren
+
+    forkChild :: IO () -> IO ThreadId
+    forkChild io = do
+      mvar <- newEmptyMVar
+      childs <- takeMVar children
+      putMVar children (mvar:childs)
+      forkOS (io >> putMVar mvar ())
 
 safeIndexResource :: Handle -> String -> IO ()
 safeIndexResource h resource = handleIOError msg $ indexResource h resource
@@ -24,13 +57,13 @@ safeIndexResource h resource = handleIOError msg $ indexResource h resource
     msg = "Cannot index resource " ++ resource
 
 indexResource :: Handle -> String -> IO ()
-indexResource handle resource =
-  runConduitRes $
-  getResourceHeadingsTrimmed resource .|
-  mapC (insertResourcePath $ pack resource) .|
-  unlinesC .|
-  encodeUtf8C .|
-  sinkHandle handle
+indexResource handle resource = runConduitRes indexingConduit >> when flushIndexedResults (hFlush handle) >> putStrLn ("Finished indexing " ++ resource)
+  where
+    indexingConduit = getResourceHeadingsTrimmed resource .|
+      mapC (insertResourcePath $ pack resource) .|
+      unlinesC .|
+      encodeUtf8C .|
+      sinkHandle handle
 
 insertResourcePath :: Text -> Text -> Text
 insertResourcePath resource heading =
