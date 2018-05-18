4f40a91fd1e2e277ad1ac7b2f54d3db93366fe3e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-05-18 09:26:18 +0200

    add HTML extracting comments

--- library/HeadingExtract/Internal/HTML.hs
+++ library/HeadingExtract/Internal/HTML.hs
@@ -4,15 +4,18 @@ module HeadingExtract.Internal.HTML
 
 import           Conduit
 import           Data.ByteString.Char8 (ByteString)
+import           Data.Char             (toLower)
 import           Data.Text             (unpack)
 import           Data.XML.Types
 import           Text.HTML.DOM
 
 import           HeadingExtract.Types
 
+-- |Parses a HTML stream and yields the headings
 getHeadingsFromHTML :: Monad m => ConduitT ByteString Heading m ()
 getHeadingsFromHTML = eventConduit .| filterHeadings .| getHeadingText
 
+-- |Retrieves the heading text from a HTML parsing event
 getHeadingText :: Monad m => ConduitT Event Heading m ()
 getHeadingText = do
   mx <- await
@@ -22,6 +25,7 @@ getHeadingText = do
     Just (EventContent (ContentEntity x)) -> yield x >> getHeadingText
     Just _                                -> getHeadingText
 
+-- |Filters the HTML events and yields elements/text inside headings
 filterHeadings :: Monad m => ConduitT Event Event m ()
 filterHeadings =
   dropWhileC (not . isEventBeginHeading) >> headC >>=
@@ -38,5 +42,6 @@ filterHeadings =
       isHeading $ unpack name
     isEventEndHeading _ = False
 
+-- |Tests whether the argument is a valid heading tag
 isHeading :: String -> Bool
-isHeading = (`elem` ["h1", "h2", "h3", "h4", "h5", "h6"])
+isHeading = (`elem` ["h1", "h2", "h3", "h4", "h5", "h6"]) . map toLower
